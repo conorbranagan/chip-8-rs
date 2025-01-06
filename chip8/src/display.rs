4e5c9c8a92e18e78b3d39e573bd85ff3be182488
Conor Branagan <conor.branagan@gmail.com>
2025-01-06 12:34:15 -0500

    break out mem and display

---
+++ chip8/src/display.rs
@@ -0,0 +1,50 @@
+const DISPLAY_WIDTH: u8 = 64;
+const DISPLAY_HEIGHT: u8 = 32;
+
+pub fn new() -> Display {
+    Display::new()
+}
+
+pub struct Display {
+    pixels: [[bool; DISPLAY_WIDTH as usize]; DISPLAY_HEIGHT as usize],
+}
+
+impl Display {
+    pub fn new() -> Display {
+        Display {
+            pixels: [[false; DISPLAY_WIDTH as usize]; DISPLAY_HEIGHT as usize],
+        }
+    }
+
+    pub fn set(&mut self, x: usize, y: usize, val: bool) {
+        if y >= self.pixels.len() || x >= self.pixels[y].len() {
+            // Cut off bytes outside of the display.
+            return;
+        }
+        self.pixels[y & (DISPLAY_HEIGHT - 1) as usize][x & (DISPLAY_WIDTH - 1) as usize] = val;
+    }
+
+    pub fn get(&mut self, x: usize, y: usize) -> Result<bool, &str> {
+        if y >= self.pixels.len() || x >= self.pixels[y].len() {
+            return Err("pixel out of range");
+        }
+        Ok(self.pixels[y][x])
+    }
+
+    pub fn clear(&mut self) {
+        self.pixels = [[false; DISPLAY_WIDTH as usize]; DISPLAY_HEIGHT as usize];
+    }
+
+    pub fn render(&mut self) {
+        for row in self.pixels.iter_mut() {
+            for v in row.iter() {
+                if *v {
+                    print!("█")
+                } else {
+                    print!(" ")
+                }
+            }
+            print!("\n");
+        }
+    }
+}
--- chip8/src/main.rs
+++ chip8/src/main.rs
@@ -2,201 +2,9 @@
 
 use rand::Rng;
 use std::env;
-use std::fs;
-use std::num::Wrapping;
 
-// Sound & Display
-
-const DISPLAY_WIDTH: u8 = 64;
-const DISPLAY_HEIGHT: u8 = 32;
-
-struct Display {
-    pixels: [[bool; DISPLAY_WIDTH as usize]; DISPLAY_HEIGHT as usize],
-}
-
-impl Display {
-    fn new() -> Display {
-        Display {
-            pixels: [[false; DISPLAY_WIDTH as usize]; DISPLAY_HEIGHT as usize],
-        }
-    }
-
-    fn set(&mut self, x: usize, y: usize, val: bool) {
-        if y >= self.pixels.len() || x >= self.pixels[y].len() {
-            // Cut off bytes outside of the display.
-            return;
-        }
-        self.pixels[y][x] = val;
-    }
-
-    fn get(&mut self, x: usize, y: usize) -> Result<bool, &str> {
-        if y >= self.pixels.len() || x >= self.pixels[y].len() {
-            return Err("pixel out of range");
-        }
-        Ok(self.pixels[y][x])
-    }
-
-    fn clear(&mut self) {
-        self.pixels = [[false; DISPLAY_WIDTH as usize]; DISPLAY_HEIGHT as usize];
-    }
-
-    fn render(&mut self) {
-        for row in self.pixels.iter_mut() {
-            for v in row.iter() {
-                if *v {
-                    print!("█")
-                } else {
-                    print!(" ")
-                }
-            }
-            print!("\n");
-        }
-    }
-}
-
-const FONT: [[u8; 5]; 16] = [
-    [0xF0, 0x90, 0x90, 0x90, 0xF0], // 0
-    [0x20, 0x60, 0x20, 0x20, 0x70], // 1
-    [0xF0, 0x10, 0xF0, 0x80, 0xF0], // 2
-    [0xF0, 0x10, 0xF0, 0x10, 0xF0], // 3
-    [0x90, 0x90, 0xF0, 0x10, 0x10], // 4
-    [0xF0, 0x80, 0xF0, 0x10, 0xF0], // 5
-    [0xF0, 0x80, 0xF0, 0x90, 0xF0], // 6
-    [0xF0, 0x10, 0x20, 0x40, 0x40], // 7
-    [0xF0, 0x90, 0xF0, 0x90, 0xF0], // 8
-    [0xF0, 0x90, 0xF0, 0x10, 0xF0], // 9
-    [0xF0, 0x90, 0xF0, 0x90, 0x90], // A
-    [0xE0, 0x90, 0xE0, 0x90, 0xE0], // B
-    [0xF0, 0x80, 0x80, 0x80, 0xF0], // C
-    [0xE0, 0x90, 0x90, 0x90, 0xE0], // D
-    [0xF0, 0x80, 0xF0, 0x80, 0xF0], // E
-    [0xF0, 0x80, 0xF0, 0x80, 0x80], // F
-];
-
-// Memory
-
-const RAM_SIZE: usize = 4 * 1024;
-const ROM_OFFSET: usize = 0x200;
-
-struct Memory {
-    data: [u8; RAM_SIZE],
-    pc: usize, // should this be separate?
-}
-
-impl Memory {
-    fn new() -> Memory {
-        Memory {
-            data: [0; RAM_SIZE],
-            pc: 0,
-        }
-    }
-
-    fn load_rom(&mut self, rom_path: &String) {
-        let rom_bytes = fs::read(rom_path).expect("unable to read rom file");
-
-        if rom_bytes.len() > (self.data.len() - ROM_OFFSET) + 1 {
-            panic!("rom is too large to fit into memory")
-        }
-        self.data[ROM_OFFSET..ROM_OFFSET + rom_bytes.len()].copy_from_slice(&rom_bytes);
-        self.pc = ROM_OFFSET;
-    }
-
-    fn read(&mut self, addr: usize) -> u8 {
-        self.data[addr]
-    }
-
-    fn set(&mut self, addr: usize, val: u8) {
-        self.data[addr] = val;
-    }
-
-    fn fetch_next(&mut self) -> Result<u16, &str> {
-        // need to read 2 bytes
-        if self.pc + 1 >= self.data.len() {
-            return Err("exceeded memory");
-        }
-        // combine to hex operation
-        let op = ((self.data[self.pc] as u16) << 8) | self.data[self.pc + 1] as u16;
-        self.pc += 2;
-        Ok(op)
-    }
-}
-
-const NUM_REGISTERS: usize = 16;
-
-struct Registers {
-    // Registers are V0 through VF
-    data: [u8; NUM_REGISTERS],
-}
-
-impl Registers {
-    fn new() -> Registers {
-        return Registers {
-            data: [0; NUM_REGISTERS],
-        };
-    }
-
-    fn get(&mut self, reg: u8) -> u8 {
-        self.data[reg as usize]
-    }
-
-    fn set(&mut self, reg: u8, val: u8) {
-        self.data[reg as usize] = val;
-    }
-
-    fn add(&mut self, reg: u8, val: u8) {
-        let reg_val = self.data[reg as usize];
-        self.data[reg as usize] = (Wrapping(reg_val) + Wrapping(val)).0;
-    }
-
-    fn sub(&mut self, reg: u8, val: u8) {
-        let reg_val = self.data[reg as usize];
-        self.data[reg as usize] = (Wrapping(reg_val) - Wrapping(val)).0;
-    }
-}
-
-// Stack
-
-static MAX_STACK_SIZE: usize = 100;
-
-struct Stack {
-    data: Vec<u16>,
-    sp: usize,
-    max_size: usize,
-}
-
-impl Stack {
-    fn new(max_size: usize) -> Stack {
-        Stack {
-            data: Vec::with_capacity(max_size),
-            sp: 0,
-            max_size,
-        }
-    }
-
-    fn push(&mut self, value: u16) {
-        if self.sp >= self.max_size {
-            // stack overflow!
-            panic!("stack overflow")
-        }
-        if self.sp == self.data.len() {
-            self.data.push(value);
-        } else {
-            self.data[self.sp] = value;
-        }
-        self.sp += 1;
-    }
-
-    fn pop(&mut self) -> u16 {
-        if self.sp == 0 {
-            // stack underflow!
-            panic!("stack underflow")
-        }
-        self.sp -= 1;
-        self.data[self.sp]
-    }
-
-    // TODO: push, pop, peek, clear?
-}
+mod display;
+mod mem;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -406,11 +214,19 @@ enum Instr {
     LoadMem { to_reg: u8 },                     // FX65
 }
 
+#[derive(Debug)]
+enum Chip8Message {
+    Tick,
+    KeyPress(u8),
+    UpdateDisplay(),
+    Shutdown,
+}
+
 struct Chip8 {
-    memory: Memory,
-    display: Display,
-    registers: Registers,
-    stack: Stack,
+    memory: mem::Memory,
+    display: display::Display,
+    registers: mem::Registers,
+    stack: mem::Stack,
     index_register: usize,
     delay_timer: u8,
     sound_timer: u8,
@@ -419,10 +235,10 @@ struct Chip8 {
 impl Chip8 {
     fn new() -> Chip8 {
         Chip8 {
-            memory: Memory::new(),
-            display: Display::new(),
-            registers: Registers::new(),
-            stack: Stack::new(MAX_STACK_SIZE),
+            memory: mem::new_memory(),
+            display: display::new(),
+            registers: mem::new_registers(),
+            stack: mem::new_stack(),
             index_register: 0,
             delay_timer: 0,
             sound_timer: 0,
@@ -453,7 +269,7 @@ impl Chip8 {
             }
             Jump { addr } => {
                 println!("Jumping to address {:#X}", addr);
-                self.memory.pc = addr as usize;
+                self.memory.set_pc(addr as usize);
             }
             CallSubroutine { addr } => {
                 println!("Calling subroutine at address {:#X}", addr);
@@ -576,7 +392,7 @@ impl Chip8 {
             }
             JumpOffset { val } => {
                 println!("Jumping to address with offset {:#X}", val);
-                self.memory.pc += self.registers.get(0) as usize;
+                self.memory.add_pc(self.registers.get(0) as usize);
             }
             Random { reg, val } => {
                 println!(
@@ -588,8 +404,8 @@ impl Chip8 {
             }
             Display { reg1, reg2, height } => {
                 // Wrap coordinates around display.
-                let x_coord = self.registers.get(reg1) & (DISPLAY_WIDTH - 1);
-                let y_coord = self.registers.get(reg2) & (DISPLAY_HEIGHT - 1);
+                let x_coord = self.registers.get(reg1);
+                let y_coord = self.registers.get(reg2);
                 println!(
                     "Displaying sprite at ({}, {}) with height {}",
                     x_coord, y_coord, height
